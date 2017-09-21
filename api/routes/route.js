7cef064f96581bda55b235e3ece85987f411e055
VinhNguyen0301 <marsino16050301zeroone@gmail.com>
2017-09-21 10:36:23 +0700

    fix name products2

--- api/routes/route.js
+++ api/routes/route.js
@@ -4,12 +4,12 @@ module.exports = function(app) {
   var todoList = require('../controllers/productController');
 
   // todoList Routes
-  app.route('/product')
+  app.route('/products')
     .get(todoList.list_all_products)
     .post(todoList.create_a_product);
 
 
-  app.route('/product/:productId')
+  app.route('/products/:productId')
     .get(todoList.read_a_product)
     .put(todoList.update_a_product)
     .delete(todoList.delete_a_product);
