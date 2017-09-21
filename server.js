5bdda7c73ede128be0ba1a570c4262373a8f07fd
nqbao3008 <nguyenquocbao3095@gmail.com>
2017-09-21 10:59:02 +0700

    add crossDomain

--- server.js
+++ server.js
@@ -19,7 +19,21 @@ user = require('./api/models/userModel'), //created model loading here
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://Tododb:123456@ds139884.mlab.com:39884/heroku_j6cj400h'); 
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
