71d648c22ff27d79834add47e7c91cc65ad123db
vmquan310 <vmquan310@gmail.com>
2017-09-25 09:59:38 +0700

    cron job

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -49,3 +49,36 @@ exports.delete_a_product = function(req, res) {
     res.json({ message: 'product successfully deleted' });
   });
 };
+
+exports.add_lazadaProduct = function(req, res){
+  request('https://www.lazada.vn/dong-ho-mat-kinh-trang-suc/?spm=a2o4n.home.0.0.bPCj8J&q=%C4%91%E1%BB%93ng+h%E1%BB%93+c%C6%A1&searchredirect=%C4%91%E1%BB%93ng+h%E1%BB%93+c%C6%A1&sort=popularity&viewType=gridView&fs=1', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    var metadata;
+    var parsedResults = [];
+    $('.c-product-card__description').each(function(i, element){
+      // Select the this element
+      var a = $(this);
+      // Parse the link title
+      var title = a.children().first().text().trim();
+      // Parse the link description
+      var description = a.children().last().text().replace(/(\r\n|\n|\r)/gm,"").trim();
+      // Parse the link price next class
+      var price = a.next().children().children().children().first().text().trim();
+           
+      // Push meta-data into parsedResults array
+      //parsedResults.push(metadata);
+      metadata = {      
+        title: title,
+        desciption: description,
+        price: price,     
+      };
+      
+      // Push meta-data into parsedResults array
+      parsedResults.push(metadata);
+    });  
+    
+    res.json(metadata);
+  }
+});
+};
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000,
-  mongoose = require('mongoose'),
+    app = express(),
+    cron = require('cron'),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    request = require('request'),
+	cheerio = require('cheerio');
 
-  product = require('./api/models/productModel'), //created model loading here
+    product = require('./api/models/productModel'), //created model loading here
 
-  category = require('./api/models/categoryModel'), //created model loading here
+    category = require('./api/models/categoryModel'), //created model loading here
 
-  bodyParser = require('body-parser');
-  
+    bodyParser = require('body-parser');
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
+mongoose.connect('mongodb://localhost/Tododb');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,3 +29,48 @@ app.listen(port);
 
 
 console.log('todo list RESTful API server started on: ' + port);
+
+var job1 = new cron.CronJob({
+  cronTime: '* * * * * *',
+  onTick: function() {
+    request('https://www.lazada.vn/dong-ho-mat-kinh-trang-suc/?spm=a2o4n.home.0.0.bPCj8J&q=%C4%91%E1%BB%93ng+h%E1%BB%93+c%C6%A1&searchredirect=%C4%91%E1%BB%93ng+h%E1%BB%93+c%C6%A1&sort=popularity&viewType=gridView&fs=1', function(error, response, html) {
+    if (!error && response.statusCode == 200) {
+        var $ = cheerio.load(html);
+        var metadata;
+        var parsedResults = [];
+        $('.c-product-card__description').each(function(i, element) {
+            // Select the this element
+            var a = $(this);
+            // Parse the link title
+            var title = a.children().first().text().trim();
+            // Parse the link description
+            var description = a.children().last().text().replace(/(\r\n|\n|\r)/gm, "").trim();
+            // Parse the link price next class
+            var price = a.next().children().children().children().first().text().trim();
+
+            // Push meta-data into parsedResults array
+            //parsedResults.push(metadata);
+            metadata = {
+                title: title,
+                desciption: description,
+                price: price,
+            };
+
+            //var json = JSON.stringify(obj)
+
+            // Push meta-data into parsedResults array
+            parsedResults.push(metadata);
+        });
+
+		//Log our finished parse results in the terminal
+    	console.log(parsedResults);
+    }
+});
+  },
+  start: false,
+  timeZone: 'Asia/Ho_Chi_Minh'
+});
+
+job1.start();
+
+
